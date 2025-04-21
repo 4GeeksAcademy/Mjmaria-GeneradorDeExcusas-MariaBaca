1b8ee5ca5fedadfcc20d8ca1866692c245fd78bb
Maria Paula Baca <mariabaca2004@gmail.com>
2025-04-21 23:17:00 +0000

    Generador de excusas MariaBaca

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  let quien = ['El gato', 'Mi hermano', 'El vecino', 'La mascota'];
+  let accion = ['saltó sobre', 'rompió', 'pintó', 'perdió'];
+  let queCosa = ['el jarrón', 'mi celular', 'la bicicleta'];
+  let cuando = [
+    'después de la lluvia',
+    'mientras trabajaba',
+    'cuando salí a comprar',
+    'en la madrugada',
+    'al terminar la reunión'
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarExcusa() {
+    let randomQuien = quien[Math.floor(Math.random() * quien.length)];
+    let randomAccion = accion[Math.floor(Math.random() * accion.length)];
+    let randomQue = queCosa[Math.floor(Math.random() * queCosa.length)];
+    let randomCuando = cuando[Math.floor(Math.random() * cuando.length)];
+    return `${randomQuien} ${randomAccion} ${randomQue} ${randomCuando}.`;
+  }
+
+  // Genera y muestra una excusa distinta en cada recarga
+  const excusa = generarExcusa();
+  document.getElementById("excuse").innerHTML = excusa;
+  console.log("Excusa generada:", excusa); // Opcional, para verificar en la consola
 };
